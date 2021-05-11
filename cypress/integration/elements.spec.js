5a703172bb20717e14f1958ff8741262ee94e040
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-05-10 23:48:49 -0300

    Manipulando Elementos

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,11 +1,49 @@
 ///<reference types = "cypress"/>
 
 describe('Work with basic elements', ()=>{
-    it('Test', ()=>{
+    
+    before(()=>{
         cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+    beforeEach(()=>{
+        cy.reload()
+    })
+
+    it('Test', ()=>{
+        
         cy.get('body').should('contain', "Cuidado")
         cy.get('span').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('contain','Cuidado')
         cy.get('.facilAchar').should('have.text','Cuidado onde clica, muitas armadilhas...')
     })
+    it('Links', ()=>{
+        
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+        cy.reload()
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+    })
+    it('TextFilds', ()=>{
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value','Cypress Test')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('???')
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto',{delay:100})
+            .should('have.value','acerto')
+            
+    })
+
 })
