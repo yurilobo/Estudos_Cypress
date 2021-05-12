2827a10c2ed1143f6bcf731eee4343299b300cb8
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-05-12 15:48:12 -0300

    Localizando elementos

---
+++ cypress/integration/base.spec.js
@@ -0,0 +1,13 @@
+///<reference types = "cypress"/>
+describe('work if alert', ()=>{
+    
+    before(()=>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+    beforeEach(()=>{
+        cy.reload()
+    })
+    it('Base', ()=>{
+
+    })
+})
