dcdd5eeb997f3b9858582f9a31a5f28453ca556c
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-05-14 19:50:03 -0300

    Verificando conta com o mesmo nome

--- cypress/integration/Barriga/functional.spec.js
+++ cypress/integration/Barriga/functional.spec.js
@@ -1,34 +1,35 @@
 ///<reference types="cypress"/>
 
 import loc from'../../support/locators'
+import '../../support/commandsContas'
 
 describe('Should test at a functional level',() =>{
     before(()=>{
         cy.login('yuri.teste@teste.com','123456')
         cy.resetApp()
-        //cy.visit('https://barrigareact.wcaquino.me/')
-        //cy.get(loc.LOGIN.USER).type('yuri.teste@teste.com')
-        //cy.get(loc.LOGIN.PASSWORD).type('123456')
-        //cy.get(loc.LOGIN.BTN_LOGIN).click()
-        //cy.get('.toast-message').should('exist')
-        //cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
+        
     })
     it('login',()=>{
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta nova teste')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta nova teste')
+        
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     })
     it('Should update an accont', ()=>{
-        //cy.get(':nth-child(7)>:nth-child(2)>.fa-edit')
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
+        cy.acessarMenuConta()
+
         cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
             .type('Conta aterada')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
+    })
+    it('Should not creat an account with same name',()=>{
+        cy.acessarMenuConta()
+
+        cy.get(loc.CONTAS.NOME).type('Conta aterada')
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain','code 400' )
     })
 })
