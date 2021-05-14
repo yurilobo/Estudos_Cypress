8473ceb01ffcb4edabd5998f0721eb48f4260d42
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-05-13 23:40:59 -0300

    Teste Loginfinaizado

--- cypress/integration/Barriga/functional.spec.js
+++ cypress/integration/Barriga/functional.spec.js
@@ -3,10 +3,13 @@
 describe('Should test at a functional level',() =>{
     before(()=>{
         cy.visit('https://barrigareact.wcaquino.me/')
-    })
-    it('login',()=>{
         cy.get('[data-test=email]').type('yuri.teste@teste.com')
         cy.get('[data-test=passwd]').type('123456')
         cy.get('.btn').click()
+        //cy.get('.toast-message').should('exist')
+        cy.get('.toast-message').should('contain', 'Bem vindo')
+    })
+    it('login',()=>{
+        
     })
 })
