a35fbcefd14e7e3759832efb61bb0d2876589b24
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-05-14 11:32:21 -0300

    Validando atuaização da conta

--- cypress/integration/Barriga/functional.spec.js
+++ cypress/integration/Barriga/functional.spec.js
@@ -1,4 +1,4 @@
-///<referencetypes="cypress"/>
+///<reference types="cypress"/>
 
 describe('Should test at a functional level',() =>{
     before(()=>{
@@ -15,5 +15,16 @@ describe('Should test at a functional level',() =>{
         cy.get('[data-test=nome]').type('Conta nova teste')
         cy.get('.btn').click()
         cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
+    })
+    it('Should update an accont', ()=>{
+        //cy.get(':nth-child(7)>:nth-child(2)>.fa-edit')
+        cy.get('[data-test=menu-settings]').click()
+        cy.get('[href="/contas"]').click()
+        cy.xpath("//table//td[contains(. , 'Conta nova teste')]/..//i[@class='far fa-edit']").click()
+        cy.get('[data-test=nome')
+            .clear()
+            .type('Conta aterada')
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'Conta atualizada com sucesso')
     })
 })
