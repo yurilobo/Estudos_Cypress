e07f742da74112603ffab38b18b4ee6f069d4155
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-05-14 21:47:51 -0300

    Inserindo movimentação

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -17,6 +17,17 @@ const locators = {
         BTN_SALVAR: '.btn',
         XP_BTN_ALTERAR: "//table//td[contains(. , 'Conta nova teste')]/..//i[@class='far fa-edit']"
     },
+    MOVIMENTACAO:{
+        DESCRICAO: '[data-test=descricao]',
+        VALOR: '[data-test=valor]',
+        INTERESSADO: '[data-test=envolvido]',
+        BTN_SALVAR: '.btn-primary'
+    },
+    EXTRATO:{
+        LINHAS: '.list-group > li',
+        XP_BUSCA_ELEMENTO: "//span[contains(., 'Desc')]/following-sibling::small[contains(., '123')]",
+    },
+    
     MESSAGE: '.toast-message'
 
 
