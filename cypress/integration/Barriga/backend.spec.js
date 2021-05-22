fd6f430d3e47928b123ab4ac091d2d6e0fc97e52
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-05-21 21:18:55 -0300

    Inseri conta repetida, inserir movimentação e saldo

--- cypress/integration/Barriga/backend.spec.js
+++ cypress/integration/Barriga/backend.spec.js
@@ -33,7 +33,23 @@ describe('Should test at a functional level', () => {
             expect(res.body).to.have.property('nome', 'Conta de teste do nome')
         })
     })
-    it('Should update an accont', () => {
+    it('Should update an account', () => {
+        cy.getContaByName('Conta para alterar')
+            .then(contaId => {
+                cy.request({
+                    url: `/contas/${contaId}`,
+                    method: 'PUT',
+                    headers: { Authorization: `JWT ${token}` },
+                    body: {
+                        nome: 'conta alterada via rest'
+                    }
+                }).as('response')
+
+            })
+
+        cy.get('@response').its('status').should('be.equal', 200)
+    })
+    it('alerar uma contas', () => {
         cy.request({
             method: 'GET',
             url:'/contas',
@@ -72,13 +88,78 @@ describe('Should test at a functional level', () => {
             expect(res.body.error).to.be.equal('Já existe uma conta com esse nome!')
         })
     })
-    it.only('Should create a transation', () => {
-        
+    it('Should create a transation', () => {
+        cy.getContaByName('Conta para movimentacoes')
+            .then(contaId => {
+                cy.request({
+                    method: 'POST',
+                    url: '/transacoes',
+                    headers: { Authorization: `JWT ${token}` },
+                    body: {
+                        conta_id: contaId,
+                        data_pagamento: Cypress.moment().add({ days: 1 }).format('DD/MM/YYYY'),
+                        data_transacao: Cypress.moment().format('DD/MM/YYYY'),
+                        descricao: "desc",
+                        envolvido: "inter",
+                        status: true,
+                        tipo: "REC",
+                        valor: "123",
+                    }
+                }).as('response')
+            })
+        cy.get('@response').its('status').should('be.equal', 201)
+        cy.get('@response').its('body.id').should('exist')
 
     })
     it('Should get balance', () => {
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+            headers: { Authorization: `JWT ${token}` }
+        }).then(res => {
+            let saldoConta = null
+            res.body.forEach(c => {
+                if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('534.00')
+        })
 
+        cy.request({
+            method: 'GET',
+            url: '/transacoes',
+            headers: { Authorization: `JWT ${token}` },
+            qs: { descricao: 'Movimentacao 1, calculo saldo' }
+        }).then(res => {
+            console.log(res.body[0])
+            cy.request({
+                url: `/transacoes/${res.body[0].id}`,
+                method: 'PUT',
+                headers: { Authorization: `JWT ${token}` },
+                body: {
+                    status: true,
+                    data_transacao: Cypress.moment(res.body[0].data_transacao).format('DD/MM/YYYY'),
+                    data_pagamento: Cypress.moment(res.body[0].data_pagamento).format('DD/MM/YYYY'),
+                    descricao: res.body[0].descricao,
+                    envolvido: res.body[0].envolvido,
+                    valor: res.body[0].valor,
+                    conta_id: res.body[0].conta_id
+                }
+            }).its('status').should('be.equal', 200)
+        })
+
+        cy.request({
+            url: '/saldo',
+            method: 'GET',
+            headers: { Authorization: `JWT ${token}` }
+        }).then(res => {
+            let saldoConta = null
+            res.body.forEach(c => {
+                if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.equal('4034.00')
+        })
     })
+    
     it('Should remover movimentação', () => {
 
     })
