f20e8a211b60528ba36b4984ed32021c538f4b8f
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-06-02 09:48:17 -0300

    Cypress Automação

---
+++ cypress/integration/CypressAutomation/FirstTest.js
@@ -0,0 +1,8 @@
+describe('Minha suite de teste', function() 
+{
+    it('Verificando titulo da pagina', function() 
+    {
+      cy.visit('https://www.nopcommerce.com/en')
+      cy.title().should('eq','Free and open-source eCommerce platform. ASP.NET based shopping cart. - nopCommerce')
+    })
+})
