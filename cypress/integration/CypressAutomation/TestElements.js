3149cb7d199b6545a64a62be413dea5ee883fec3
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-06-03 18:26:43 -0300

    Preechendo elementos tipo texto

--- cypress/integration/CypressAutomation/TestElements.js
+++ cypress/integration/CypressAutomation/TestElements.js
@@ -8,11 +8,26 @@ describe('Ui Elements', function()
 
             cy.get(':nth-child(1) > :nth-child(2) > .form-control')
                 .type('Yuri Anderson ')
-                .should('have.value', 'Yuri Anderson ')
+                //.should('have.value', 'Yuri Anderson ')
+                .should('be.visible')
                 
             cy.get(':nth-child(1) > :nth-child(3) > .form-control')
                 .type('Lobo')
-                .should('have.value', 'Lobo')
+                //.should('have.value', 'Lobo')
+                .should('be.visible')
+
+            cy.get('.col-md-8 > .form-control')
+                .type('Este é um teste de como preencher um formulario e ultilizar o cypress')
+                //.should('have.value', 'Lobo')
+                .should('be.visible')
+
+            cy.get('#eid > .form-control')
+                .type('teste@teste.com')
+                .should('be.visible')
+            
+            cy.get(':nth-child(4) > .col-md-4 > .form-control')
+                .type('9999999999')
+                .should('be.visible')
 
         }
             
