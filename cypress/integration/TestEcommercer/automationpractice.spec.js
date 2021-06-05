44d025be62b584c61b74245bbf6d002e12d12022
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-06-04 21:11:13 -0300

    Compeendendo o um ecomeerceecomeerce

---
+++ cypress/integration/TestEcommercer/automationpractice.spec.js
@@ -0,0 +1,16 @@
+///<reference types="cypress"/>
+
+describe('Minha suite de teste', function() 
+{
+    it('Verificando ecommercer', function() 
+    {
+      cy.visit('http://automationpractice.com/index.php')
+      cy.title().should('eq','My Store')
+    })
+    it('procurando elemento', function() 
+    {
+        cy.get('#search_query_top').click().type('Faded Short Sleeve')
+        cy.get('#searchbox > .btn').click()
+
+    })
+})
