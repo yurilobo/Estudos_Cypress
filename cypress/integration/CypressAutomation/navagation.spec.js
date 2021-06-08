8ca5ed1cc03cb1080ca7e8df1f50717fc59677b1
Yuri Anderson <yuri.anderson.lobo@gmail.com>
2021-06-08 11:23:42 -0300

    Navegaão na pagina

--- cypress/integration/CypressAutomation/navagation.spec.js
+++ cypress/integration/CypressAutomation/navagation.spec.js
@@ -5,5 +5,23 @@ describe('Minha suite de teste', function()
     {
       cy.visit('https://www.nopcommerce.com/en')
       cy.title().should('eq','Free and open-source eCommerce platform. ASP.NET based shopping cart. - nopCommerce')
+      
+      cy.get('.userlink').click()
+      cy.get('.user-wrapper > :nth-child(2)').click()
+      cy.title().should('eq','Register - nopCommerce')//reg page
+
+      cy.go('back')
+      cy.title().should('eq','Free and open-source eCommerce platform. ASP.NET based shopping cart. - nopCommerce')//home
+
+      cy.go('forward')
+      cy.title().should('eq','Register - nopCommerce')//reg page
+
+      cy.go(-1)
+      cy.title().should('eq','Free and open-source eCommerce platform. ASP.NET based shopping cart. - nopCommerce')//home
+
+      cy.go(1)
+      cy.title().should('eq','Register - nopCommerce')//reg page
+      
+      cy.reload()
     })
 })
